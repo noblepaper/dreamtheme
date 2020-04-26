2dc67cc25a22f30c1ff51fc0e322a55c169708d4
Kit Masaracchia <kit.masaracchia@gmail.com>
2020-04-25 18:40:58 -0600

    basic tab routing

--- app/components/LoginForm/index.js
+++ app/components/LoginForm/index.js
@@ -5,12 +5,14 @@
  */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { colors, fonts, media } from 'theme';
+import _ from 'lodash';
 
 import SignInTab from './SignInTab';
+import RegisterTab from './RegisterTab';
 
 const Wrapper = styled.form`
   padding: 37px 0 65px;
@@ -35,7 +37,7 @@ const TabHeader = styled.div`
   }
 `;
 
-const OnTab = styled.h1`
+const TabSelected = styled.h1`
   ${fonts.default.light}
   color: ${colors.font};
   font-size: 32px;
@@ -44,7 +46,7 @@ const OnTab = styled.h1`
   vertical-align: middle;
 `;
 
-const SwitchToTab = styled.a`
+const SwitchToTab = styled(Link)`
   ${fonts.default.light}
   color: ${colors.font};
   margin-bottom: 52px;
@@ -64,12 +66,16 @@ const ContentWrapper = styled.div`
 
 function LoginForm({ loginWithGoogle }) {
   const loginFunctions = { loginWithGoogle };
+  const isLogin = _.includes(window.location.pathname, 'login');
+  const selectedLabel = isLogin ? 'Sign In' : 'Register';
+  const switchToLabel = isLogin ? 'Register' : 'Sign In';
+  const switchToPath = isLogin ? '/user/register' : '/user/login';
 
   return (
     <Wrapper>
       <TabHeader>
-        <OnTab>Sign In</OnTab>
-        <SwitchToTab>Register</SwitchToTab>
+        <TabSelected>{selectedLabel}</TabSelected>
+        <SwitchToTab to={switchToPath}>{switchToLabel}</SwitchToTab>
       </TabHeader>
       <ContentWrapper>
         <Switch>
@@ -78,6 +84,7 @@ function LoginForm({ loginWithGoogle }) {
             path="/user/login"
             render={() => <SignInTab loginFunctions={loginFunctions} />}
           />
+          <Route exact path="/user/register" render={() => <RegisterTab />} />
         </Switch>
       </ContentWrapper>
     </Wrapper>
