98c361117fa19d47dbb37473e7d86c46c3d196a1
Kit Masaracchia <kit.masaracchia@gmail.com>
2020-05-03 21:53:07 -0600

    get color picking working

--- app/components/ColorPicker/index.js
+++ app/components/ColorPicker/index.js
@@ -11,8 +11,17 @@ import styled from 'styled-components';
 const Canvas = styled.canvas`
   margin: 50px;
   border-radius: 10px;
-  /* background-color: red; */
-  border: 1px solid #c4c4c4;
+  border: 1px solid #e4e4e4;
+  display: inline-block;
+`;
+
+const CurrentColor = styled.div`
+  display: inline-block;
+  height: 75px;
+  width: 75px;
+  border-radius: 75px;
+  background-color: ${props => props.fill};
+  transition: background-color 0.25s ease;
 `;
 
 let context = null;
@@ -22,29 +31,35 @@ function ColorPicker() {
   const canvasRef = useRef();
 
   // state
-  const [color, setColor] = useState('#BAD577');
+  const [hue, setHue] = useState('#BAD577');
+  const [currentColor, setCurrentColor] = useState(hue);
 
   // lifecycle
   const useMountEffect = () =>
     useEffect(() => {
       context = canvasRef.current && canvasRef.current.getContext('2d');
-
-      const hueGradient = context.createLinearGradient(0, 0, 350, 0);
-      hueGradient.addColorStop(1, color);
-      hueGradient.addColorStop(0, 'rgba(0,0,0,0)');
-
-      const greyscaleGradient = context.createLinearGradient(0, 400, 0, 0);
-      greyscaleGradient.addColorStop(1, 'rgba(255,255,255,0)');
-      greyscaleGradient.addColorStop(0, 'rgba(0,0,0,1)');
-
-      context.fillStyle = hueGradient;
-      context.fillRect(0, 0, 400, 350);
-      context.fillStyle = greyscaleGradient;
-      context.fillRect(0, 0, 400, 350);
     });
   useMountEffect();
 
-  const onCanvasClick = event => {
+  useEffect(() => {
+    const hueGradient = context.createLinearGradient(0, 0, 350, 0);
+    hueGradient.addColorStop(1, hue);
+    hueGradient.addColorStop(0, 'rgba(255,255,255,1)');
+
+    // TODO: move greyscale to its own component bc it doesn't need to change
+    const greyscaleGradient = context.createLinearGradient(0, 400, 0, 0);
+    greyscaleGradient.addColorStop(1, 'rgba(255,255,255,0)');
+    greyscaleGradient.addColorStop(0, 'rgba(0,0,0,1)');
+
+    context.clearRect(0, 0, 400, 350);
+    context.fillStyle = hueGradient;
+    context.fillRect(0, 0, 400, 350);
+    context.fillStyle = greyscaleGradient;
+    context.fillRect(0, 0, 400, 350);
+  }, [hue]);
+
+  // methods
+  const pickAColor = event => {
     event.persist();
     const { left, top } = canvasRef.current.getBoundingClientRect();
     const x = event.pageX - left;
@@ -52,23 +67,14 @@ function ColorPicker() {
     const colorData = context.getImageData(x, y, 1, 1).data;
     const [r, g, b, a] = colorData;
     const newColor = `rgba(${r}, ${g}, ${b}, ${a})`;
-
-    const hueGradient = context.createLinearGradient(0, 0, 350, 0);
-    hueGradient.addColorStop(1, newColor);
-    hueGradient.addColorStop(0, 'rgba(0,0,0,0)');
-
-    const greyscaleGradient = context.createLinearGradient(0, 400, 0, 0);
-    greyscaleGradient.addColorStop(1, 'rgba(255,255,255,0)');
-    greyscaleGradient.addColorStop(0, 'rgba(0,0,0,1)');
-
-    context.fillStyle = hueGradient;
-    context.fillRect(0, 0, 400, 350);
-    context.fillStyle = greyscaleGradient;
-    context.fillRect(0, 0, 400, 350);
+    setCurrentColor(newColor);
   };
 
   return (
-    <Canvas width="400" height="350" ref={canvasRef} onClick={onCanvasClick} />
+    <>
+      <Canvas width="400" height="350" ref={canvasRef} onClick={pickAColor} />
+      <CurrentColor fill={currentColor} />
+    </>
   );
 }
 
